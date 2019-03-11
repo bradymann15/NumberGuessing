b5df3b7cb0087aa9d9d9daee99f0039b3f17aba1
Brady Mann <Football3320>
2019-03-11 14:10:10 -0400

    declared and initialized the random number

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -3,6 +3,10 @@ package com.LickingHeights;
 public class Main {
 
     public static void main(String[] args) {
-	
+
+        int pickedNumber;
+
+        pickedNumber = (int)(Math.random()*100);
+
     }
 }
