40339155ea5d30899fe88503727c762554a27b78
Brady Mann <Football3320>
2019-03-11 14:15:52 -0400

    declared and initialized the scanner and first guess

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,14 +1,19 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
         int pickedNumber;
-
+        Scanner keyboard;
+        keyboard = new Scanner(System.in);
         pickedNumber = (int)(Math.random()*100);
 
         System.out.println("Guess a number between 1 and 100");
+        int guess1;
+        guess1 = keyboard.nextInt();
 
     }
 }
