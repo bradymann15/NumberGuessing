6c526055381198d1476c5312ff5f5baf71d74b25
Brady Mann <Football3320>
2019-03-11 14:18:30 -0400

    reminder

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
+        //remind me to make a method for the conditionals
         int pickedNumber;
         Scanner keyboard;
         keyboard = new Scanner(System.in);
