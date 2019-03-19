885640d77e10073e236f6e7176214aa725f642e8
Brady Mann <Football3320>
2019-03-19 14:04:13 -0400

    finished the method and the rest of the guessing game

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -13,13 +13,29 @@ public class Main {
         System.out.println("Guess a number between 1 and 100");
         int guess1;
         guess1 = keyboard.nextInt();
+        System.out.println("Guess again! Two more left!");
+        guess1= keyboard.nextInt();
+        System.out.println("Guess again! One more guess!");
+        guess1= keyboard.nextInt();
+        System.out.println("Sorry, but the number was "+pickedNumber+". Thanks for playing!");
+
 
 
 
         }
     public static int guess(int pickedNumber, int guess1){
-        if (guess1==pickedNumber)
-            return "Correct"
+        if (pickedNumber>guess1){
+            System.out.println("Incorrect! The number you guessed is too small.");
+        }
+        else if (pickedNumber<guess1){
+            System.out.println();
+        }
+        else {
+            System.out.println("Correct! The number was "+pickedNumber+". Thanks for playing!");
+        System.exit(0);
+        }
+
+
 
     }
         }
