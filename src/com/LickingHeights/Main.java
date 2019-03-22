247db6fbaf6bf27ecb2b4e913625238ba02485e6
Brady Mann <Football3320>
2019-03-22 13:44:08 -0400

    Fixed the numberguessing method and completed project

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -2,7 +2,7 @@ package com.LickingHeights;
 
 import java.util.Scanner;
 
-public class Main {
+class Main {
 
     public static void main(String[] args) {
         int pickedNumber;
@@ -10,28 +10,32 @@ public class Main {
         keyboard = new Scanner(System.in);
         pickedNumber = (int)(Math.random()*100);
 
-        System.out.println("Guess a number between 1 and 100");
+        System.out.println("Guess a number between 0 and 100");
         int guess1;
         guess1 = keyboard.nextInt();
+        guess(pickedNumber,guess1);
         System.out.println("Guess again! Two more left!");
         guess1= keyboard.nextInt();
+        guess(pickedNumber,guess1);
         System.out.println("Guess again! One more guess!");
         guess1= keyboard.nextInt();
+        guess(pickedNumber,guess1);
         System.out.println("Sorry, but the number was "+pickedNumber+". Thanks for playing!");
 
 
 
 
         }
-    public static String guess(int pickedNumber, int guess1){
+    public static void guess(int pickedNumber, int guess1){
         if (pickedNumber>guess1){
-            return "Incorrect! The number you guessed is too small.";
+             System.out.println("Incorrect! The number you guessed is too small");
         }
         else if (pickedNumber<guess1){
-            return "Incorrect! The number you guessed is too big";
+            System.out.println("Incorrect! The number you guessed is too big");
         }
         else {
-            return "Correct! The number was "+pickedNumber+". Thanks for playing!";
+            System.out.println("Correct! The number was "+pickedNumber+". Thanks for playing!");
+            System.exit(0);
 
         }
     }
